923c80d3406816ce1c7f261845850693a039639f
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-01-08 17:11:14 -0500

    lookup

--- adaptors/actionkit/events.js
+++ adaptors/actionkit/events.js
@@ -108,7 +108,7 @@ function configureOsdify(api, config) {
     const signups = await api.get('eventsignup').query({ event: ak.id })
     const attendance_count = signups.body.meta.total_count
 
-    const time_zone = zipcode_to_timezone(ak.zip)
+    const time_zone = zipcode_to_timezone.lookup(ak.zip)
 
     return {
       id: ak.id,
