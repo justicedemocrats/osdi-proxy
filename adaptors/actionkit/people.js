e57f5211c1bfc3a144263ad04507d28eb8e91b02
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-02-16 11:24:24 -0500

    add postal addresses to ak people

--- adaptors/actionkit/people.js
+++ adaptors/actionkit/people.js
@@ -1,30 +1,42 @@
-const cacher = require('../../lib').cacher('ak-people')
+const cacher = require("../../lib").cacher("ak-people");
 
 const osdiify = async (api, ak) => {
-  const phone_numbers = []
-  const phone_id = ak.phones[0] ? ak.phones[0].split('/')[4] : false
+  const phone_numbers = [];
+  const phone_id = ak.phones[0] ? ak.phones[0].split("/")[4] : false;
 
   if (phone_id) {
-    const result = await api.get(`phone/${phone_id}`)
-    phone_numbers.push({ number: result.body.normalized_phone })
+    const result = await api.get(`phone/${phone_id}`);
+    phone_numbers.push({ number: result.body.normalized_phone });
   }
+
+  const postal_addresses = [
+    {
+      primary: true,
+      address_lines: [ak.address1, ak.address2].filter(al => al != ""),
+      locality: ak.city,
+      region: ak.state,
+      postal_code: ak.zip,
+      country: ak.country
+    }
+  ];
 
   return {
     family_name: ak.last_name,
     given_name: ak.first_name,
     email_addresses: [{ address: ak.email }],
-    phone_numbers
-  }
-}
+    phone_numbers,
+    postal_addresses
+  };
+};
 
 module.exports = api => ({
   one: async id => {
     return await cacher.fetch_and_update(
       id,
       (async () => {
-        const result = await api.get(`user/${id}`)
-        return await osdiify(api, result.body)
+        const result = await api.get(`user/${id}`);
+        return await osdiify(api, result.body);
       })()
-    )
+    );
   }
-})
+});
