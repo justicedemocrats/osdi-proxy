c859bdb4d59e5a21d5f0682e03dab7b844be143d
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-02-16 11:53:15 -0500

    log headers

--- secret.js
+++ secret.js
@@ -1,6 +1,7 @@
 const secret = process.env.OSDI_API_TOKEN;
 
 module.exports = (req, res, next) => {
+  console.log(req.headers);
   const header = req.headers["OSDI-API-Token"] || req.headers["osdi-api-token"];
 
   if ((header && header == secret) || req.path == "/") {
