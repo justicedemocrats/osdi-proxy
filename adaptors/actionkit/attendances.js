314f22f42bff01c2b161140413729b7e11091d8e
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-02-16 21:50:31 -0500

    fix linked resource query bug

--- adaptors/actionkit/attendances.js
+++ adaptors/actionkit/attendances.js
@@ -1,52 +1,52 @@
-const cacher = require('../../lib').cacher('ak-attendance')
+const cacher = require("../../lib").cacher("ak-attendance");
 
 const osdiify = async (api, ak) => {
   return {
     attended: ak.attended,
-    person: ak.user.split('/')[4]
-  }
-}
+    person: ak.user.split("/")[4]
+  };
+};
 
 const akify = async (api, osdi, config = {}) => {
   return {
     attended: osdi.attended
-  }
-}
+  };
+};
 
 module.exports = api => ({
-  count: async ({ event }) => {
-    const reference = event
+  count: async params => {
+    const event_id = params.event || params.events;
     return await cacher.fetch_and_update(
-      reference,
+      `count-attendances-${event_id}`,
       (async () => {
-        const result = await api.get('eventsignup').query({ event })
-        return result.body.meta.total_count
+        const result = await api.get("eventsignup").query({ event: event_id });
+        return result.body.meta.total_count;
       })()
-    )
+    );
   },
 
   findAll: async params => {
-    const page = (params && params.page) || 0
-    const reference = `all-${page}`
+    const page = (params && params.page) || 0;
+    const reference = `all-${page}`;
 
     const results = await api
-      .get('eventsignup')
-      .query({ event: params.event, _offset: 100 * page, _limit: 100 })
+      .get("eventsignup")
+      .query({ event: params.event, _offset: 100 * page, _limit: 100 });
 
     return await Promise.all(
       results.body.objects.map(obj => osdiify(api, obj))
-    )
+    );
   },
 
   one: async id => {
     return await cacher.fetch_and_update(
       id,
       (async () => {
-        const result = await api.get(`eventsignup${id}`)
-        return await osdiify(result)
+        const result = await api.get(`eventsignup${id}`);
+        return await osdiify(result);
       })()
-    )
-  },
+    );
+  }
 
   // create: async object => {
   //   return await api.post('eventsignupaction').send(akify(object))
@@ -60,4 +60,4 @@ module.exports = api => ({
   // delete: async id => {
   //   return await api.put(`delete/${id}`)
   // }
-})
+});
