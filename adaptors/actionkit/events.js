b8b8df5512fb5b4ea17aea3214d103689261f8b1
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-02-22 16:58:26 -0500

    format end date

--- adaptors/actionkit/events.js
+++ adaptors/actionkit/events.js
@@ -143,10 +143,16 @@ function configureOsdify(api, config) {
       browser_url: config.eventUrlBase + `/${ak.id}`,
       name: ak.title ? ak.title.toLowerCase().replace(/ /g, "-") : undefined,
       title: ak.title,
-      start_date: time_zone
-        ? moment.tz(ak.starts_at, time_zone).format()
-        : ak.starts_at,
-      end_date: time_zone ? moment.tz(ak.ends_at, time_zone) : ak.ends_at,
+
+      start_date:
+        time_zone && time_zone != ""
+          ? moment.tz(ak.starts_at, time_zone).format()
+          : ak.starts_at,
+      end_date:
+        time_zone && time_zone != ""
+          ? moment.tz(ak.ends_at, time_zone).format()
+          : ak.ends_at,
+
       attendance_count: attendance_count,
       description: ak.public_description,
       instructions: ak.directions,
