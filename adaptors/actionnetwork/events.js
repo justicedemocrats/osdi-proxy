b76c0295e0be2b2994d9e9154aa8ac46eafaf1c6
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-03-21 13:57:24 -0400

    default end dates for actionnetwork

--- adaptors/actionnetwork/events.js
+++ adaptors/actionnetwork/events.js
@@ -1,3 +1,5 @@
+const moment = require("moment");
+
 const transform_event = config => event => {
   const id = event.identifiers[0].split(":")[1];
   const organizer = event._embedded["osdi:organizer"];
@@ -10,6 +12,13 @@ const transform_event = config => event => {
       email_address: organizer.email_addresses[0].address
     }
   };
+
+  if (!event.end_date) {
+    new_props.end_date = moment(event.start_date)
+      .add(3, "hours")
+      .toDate()
+      .toISOString();
+  }
 
   return Object.assign({}, event, new_props);
 };
