215822e7955ea51a6eb93a7c910f2ee99ff36617
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-05-08 09:51:43 -0400

    event_id

--- adaptors/actionnetwork/record_attendance_helper.js
+++ adaptors/actionnetwork/record_attendance_helper.js
@@ -2,7 +2,9 @@ module.exports = (api, config) => {
   return {
     create: async (params, object) => {
       const event_id = params.event || params.events;
-      const result = await api.post(`events/${id}/attendances`).send(object);
+      const result = await api
+        .post(`events/${event_id}/attendances`)
+        .send(object);
       return result.body;
     }
   };
